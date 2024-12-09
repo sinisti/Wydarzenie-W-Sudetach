bfdb27e8b7724e57526ad700d519318453912681
sinisti <spideralan2008@gmail.com>
2024-12-09 09:08:07 +0100

    commit

---
+++ app/src/main/java/com/example/wydarzeniewsudetach/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.wydarzeniewsudetach;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView counter;
+    private TextView info;
+    private Button likeb;
+    private Button remove;
+    private Button seeu;
+    private Button save;
+    private EditText ETemail;
+    private EditText ETpass;
+    private EditText ETrpass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
