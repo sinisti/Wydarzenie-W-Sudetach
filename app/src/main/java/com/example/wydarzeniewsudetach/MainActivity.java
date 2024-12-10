8e9cfd95795442e1f69589906b5f9d0512cb5640
sinisti <spideralan2008@gmail.com>
2024-12-10 11:36:01 +0100

    UWUUWUWUWUUWUWUW

--- app/src/main/java/com/example/wydarzeniewsudetach/MainActivity.java
+++ app/src/main/java/com/example/wydarzeniewsudetach/MainActivity.java
@@ -20,15 +20,56 @@ public class MainActivity extends AppCompatActivity {
     private EditText ETemail;
     private EditText ETpass;
     private EditText ETrpass;
+    public int count=0;
+    public String emailr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        likeb = findViewById(R.id.likeb);
+        remove = findViewById(R.id.remove);
+        seeu = findViewById(R.id.seeu);
+        save = findViewById(R.id.save);
+        counter = findViewById(R.id.counter);
+        info = findViewById(R.id.info);
+        ETemail = findViewById(R.id.ETemail);
+        ETpass = findViewById(R.id.ETpass);
+        ETrpass = findViewById(R.id.ETrpass);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        likeb.setOnClickListener(v -> addLike());
+        remove.setOnClickListener(v -> removeLike());
+        save.setOnClickListener(v -> saveinfo());
+        seeu.setOnClickListener(v -> showinfo());
+    }
+    void addLike(){
+        count += 1;
+        counter.setText(count + " polubień");
+    }
+    void removeLike(){
+        if (count>0) {
+            count -= 1;
+            counter.setText(count + " polubień");
+        }
+    }
+    void saveinfo(){
+        if(ETemail.getText().toString().contains("@")){
+            if(ETpass.getText().toString().equals(ETrpass.getText().toString())){
+                info.setText("Zarejestrowano " + ETemail.getText().toString());
+                emailr = ETemail.getText().toString();
+            }
+            else{
+                info.setText("Hasła się różnią");
+            };
+        }else{
+            info.setText("Nieprawidłowy adres e-mail");
+        }
+    }
+    void showinfo(){
+        info.setText(emailr);
     }
 }
